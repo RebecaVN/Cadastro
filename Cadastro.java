0e007476ba803773e8201efaf54be0f12964c14a
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 08:40:17 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,15 @@
+package Atividade1;
+
+public class Cadastro {
+	public static void main(String[] args) {
+		
+	}
+	public String nome;
+	public String estado;
+	public String numeroFuncionario;
+	public String faturamentoMensal;
+}
+
+
+//-------------------------------------------------------------------------------------------//
+
